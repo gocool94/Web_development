b2ba619657a06ab9f06a57bf59dfcc0ce3d5d101
gocool94 <31814599+gocool94@users.noreply.github.com>
2021-04-03 16:20:39 +0530

    added exercise

--- Advanced Javascript/advanced_exercise_1/exercise1.js
+++ Advanced Javascript/advanced_exercise_1/exercise1.js
@@ -44,3 +44,133 @@ if (true) {
     alert(a);
 }
 alert(a);
+
+
+//#1 change this function into a ternary and assign it to variable called experiencePoints
+function experiencePoints() {
+    if (winBattle()) {
+        return 10;
+    } else {
+        return 1;
+    }
+}
+
+//Using this function, answer the questions below:
+function moveCommand(direction) {
+    var whatHappens;
+    switch (direction) {
+        case "forward":
+            break;
+            whatHappens = "you encounter a monster";
+        case "back":
+            whatHappens = "you arrived home";
+            break;
+            break;
+        case "right":
+            return whatHappens = "you found a river";
+            break;
+        case "left":
+            break;
+            whatHappens = "you run into a troll";
+            break;
+        default:
+            whatHappens = "please enter a valid direction";
+    }
+    return whatHappens;
+}
+
+//#2 return value when moveCommand("forward");
+
+//#3 return value when moveCommand("back");
+
+//#4 return value when moveCommand("right");
+
+//#5 return value when moveCommand("left");
+
+//BONUS: practice makes perfect. Go and write your own switch function. It takes time to get used to the syntax!
+
+// change everything below to the newer Javascript!
+
+// let + const
+var a = 'test';
+var b = true;
+var c = 789;
+a = 'test2';
+
+
+// Destructuring
+var person = {
+    firstName : "John",
+    lastName  : "Doe",
+    age       : 50,
+    eyeColor  : "blue"
+};
+
+var firstName = person.firstName;
+var lastName = person.lastName;
+var age = person.age;
+var eyeColor = person.eyeColor;
+
+
+// Object properties
+var a = 'test';
+var b = true;
+var c = 789;
+
+var okObj = {
+  a: a,
+  b: b,
+  c: c
+};
+
+
+// Template strings
+var message = "Hello " + firstName + " have I met you before? I think we met in " + city + " last summer no???";
+
+
+// default arguments
+// default age to 10;
+function isValidAge(age) {
+    return age
+}
+
+// Symbol
+// Create a symbol: "This is my first Symbol"
+
+// Arrow functions
+function whereAmI(username, location) {
+    if (username && location) {
+        return "I am not lost";
+    } else {
+        return "I am totally lost!";
+    }
+}
+
+//Solve these problems:
+
+//#1 Create a one line function that adds adds two parameters
+
+//Closure: What does the last line return?
+const addTo = x => y => x + y
+var addToTen = addTo(10)
+addToTen(3)
+
+//Currying: What does the last line return?
+const sum = (a, b) => a + b
+const curriedSum = (a) => (b) => a + b
+curriedSum(30)(1)
+
+
+//Currying: What does the last line return?
+const sum = (a, b) => a + b
+const curriedSum = (a) => (b) => a + b
+const add5 = curriedSum(5)
+add5(12)
+
+//Composing: What does the last line return?
+const compose = (f, g) => (a) => f(g(a));
+const add1 = (num) => num + 1;
+const add5 = (num) => num + 5;
+compose(add1, add5)(10)
+
+//What are the two elements of a pure function?
